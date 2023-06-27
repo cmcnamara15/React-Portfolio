dd2c2488225105b005526e0c951dd5ba8cf1716e
Chris McNamara <cmcnamara1991@icloud.com>
2023-06-27 16:22:31 -0500

    Add resume photo to resume page

--- react-portfolio/src/pages/Resume.jsx
+++ react-portfolio/src/pages/Resume.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import resumeImageOne from "../assets/images/resumeOne.png";
+// import resumeImageTwo from "../assets/images/resumeTwo.png";
+// import resumePdf from "../assets/resume.pdf";
 
 export default function Resume() {
     return (
-    <div>
-        <h1>Resume</h1>
-    </div>
+<div>
+    <h1>My Resume</h1>
+    <img src={resumeImageOne} alt="Resume" style={{ width: '75%', height: 'auto' }} />
+    {/* <a href={resumePdf} download="resume.pdf">Download PDF</a> */}
+</div>
     )
 }
