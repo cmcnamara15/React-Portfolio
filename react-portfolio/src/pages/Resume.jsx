8fe23826529d16272d1304131404fc35f5852671
Chris McNamara <cmcnamara1991@icloud.com>
2023-06-27 16:23:22 -0500

    Adjust size of resume png onresume page

--- react-portfolio/src/pages/Resume.jsx
+++ react-portfolio/src/pages/Resume.jsx
@@ -7,7 +7,7 @@ export default function Resume() {
     return (
 <div>
     <h1>My Resume</h1>
-    <img src={resumeImageOne} alt="Resume" style={{ width: '75%', height: 'auto' }} />
+    <img src={resumeImageOne} alt="Resume" style={{ width: '50%', height: 'auto' }} />
     {/* <a href={resumePdf} download="resume.pdf">Download PDF</a> */}
 </div>
     )
