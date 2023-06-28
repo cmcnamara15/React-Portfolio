aa805e819c480b202b4e5ba90777fd318f96298e
Chris McNamara <cmcnamara1991@icloud.com>
2023-06-28 14:10:50 -0500

    move photData to seperate file to make code more readable

--- react-portfolio/src/pages/Photos.jsx
+++ react-portfolio/src/pages/Photos.jsx
@@ -3,93 +3,7 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css'; // This imports the lightbox CSS.
-import horse from "../assets/photography/horse.JPG";
-import galicia from "../assets/photography/galicia.JPG";
-import grandmaFlower from "../assets/photography/grandmaFlower.JPG";
-import et from "../assets/photography/etPondering.JPG";
-import moon from "../assets/photography/moon.JPG";
-import sunset from "../assets/photography/sunsetSeagull.JPG";
-import vanGough from "../assets/photography/vanGoughTree.JPG";
-import cross from "../assets/photography/cross.JPG";
-import stump from "../assets/photography/stumps.JPG";
-import rearView from "../assets/photography/rearView.JPG";
-import lights from "../assets/photography/lights.JPG";
-import van from "../assets/photography/van.JPG"
-
-const itemData = [
-    {
-        img: horse,
-        title: 'Image 1',
-        cols: 1,
-        rows: 1,
-    },
-    {
-        img: galicia,
-        title: 'Image 2',
-        cols: 2,
-        rows: 1,
-    },
-    {
-        img: grandmaFlower,
-        title: 'Image 3',
-        cols: 1,
-        rows: 2,
-    },
-    {
-        img: et,
-        title: 'Image 4',
-        cols: 1,
-        rows: 1,
-    },
-    {
-        img: moon,
-        title: 'Image 5',
-        cols: 1,
-        rows: 1,
-    },
-    {
-        img: sunset,
-        title: 'Image 6',
-        cols: 2,
-        rows: 1,
-    },
-    {
-        img: vanGough,
-        title: 'Image 7',
-        cols: 1,
-        rows: 2,
-    },
-    {
-        img: cross,
-        title: 'Image 8',
-        cols: 1,
-        rows: 1,
-    },
-    {
-        img: lights,
-        title: 'Image 9',
-        cols: 1,
-        rows: 1,
-    },
-    {
-        img: rearView,
-        title: 'Image 10',
-        cols: 2,
-        rows: 1,
-    },
-    {
-        img: stump,
-        title: 'Image 11',
-        cols: 1,
-        rows: 2,
-    },
-    {
-        img: van,
-        title: 'Image 14',
-        cols: 2,
-        rows: 1,
-    },
-]
+import photoData from './photoData';
 
 
 export default function Photos() {
@@ -101,9 +15,9 @@ export default function Photos() {
             <ImageList
                 sx={{ maxWidth: '100%', height: 'auto' }}
                 cols={3}
-                rowHeight={190}
+                rowHeight={164}
             >
-                {itemData.map((item, index) => (
+                {photoData.map((item, index) => ( // Use photoData here instead of itemData
                     <ImageListItem key={item.img} onClick={() => {setPhotoIndex(index); setIsOpen(true)}}>
                         <img
                             src={item.img}
@@ -115,15 +29,15 @@ export default function Photos() {
             </ImageList>
             {isOpen && (
                 <Lightbox
-                    mainSrc={itemData[photoIndex].img}
-                    nextSrc={itemData[(photoIndex + 1) % itemData.length].img}
-                    prevSrc={itemData[(photoIndex + itemData.length - 1) % itemData.length].img}
+                    mainSrc={photoData[photoIndex].img}
+                    nextSrc={photoData[(photoIndex + 1) % photoData.length].img}
+                    prevSrc={photoData[(photoIndex + photoData.length - 1) % photoData.length].img}
                     onCloseRequest={() => setIsOpen(false)}
                     onMovePrevRequest={() =>
-                setPhotoIndex((photoIndex + itemData.length - 1) % itemData.length)
+                setPhotoIndex((photoIndex + photoData.length - 1) % photoData.length)
                 }
                 onMoveNextRequest={() =>
-                    setPhotoIndex((photoIndex + 1) % itemData.length)
+                    setPhotoIndex((photoIndex + 1) % photoData.length)
                 }
                 />
             )}
---
+++ react-portfolio/src/pages/photoData.js
@@ -0,0 +1,89 @@
+import horse from "../assets/photography/horse.JPG";
+import galicia from "../assets/photography/galicia.JPG";
+import grandmaFlower from "../assets/photography/grandmaFlower.JPG";
+import et from "../assets/photography/etPondering.JPG";
+import moon from "../assets/photography/moon.JPG";
+import sunset from "../assets/photography/sunsetSeagull.JPG";
+import vanGough from "../assets/photography/vanGoughTree.JPG";
+import cross from "../assets/photography/cross.JPG";
+import stump from "../assets/photography/stumps.JPG";
+import rearView from "../assets/photography/rearView.JPG";
+import lights from "../assets/photography/lights.JPG";
+import van from "../assets/photography/van.JPG"
+
+const photoData = [
+    {
+        img: horse,
+        title: 'Image 1',
+        cols: 1,
+        rows: 1,
+    },
+    {
+        img: galicia,
+        title: 'Image 2',
+        cols: 2,
+        rows: 1,
+    },
+    {
+        img: grandmaFlower,
+        title: 'Image 3',
+        cols: 1,
+        rows: 2,
+    },
+    {
+        img: et,
+        title: 'Image 4',
+        cols: 1,
+        rows: 1,
+    },
+    {
+        img: moon,
+        title: 'Image 5',
+        cols: 1,
+        rows: 1,
+    },
+    {
+        img: sunset,
+        title: 'Image 6',
+        cols: 2,
+        rows: 1,
+    },
+    {
+        img: vanGough,
+        title: 'Image 7',
+        cols: 1,
+        rows: 2,
+    },
+    {
+        img: cross,
+        title: 'Image 8',
+        cols: 1,
+        rows: 1,
+    },
+    {
+        img: lights,
+        title: 'Image 9',
+        cols: 1,
+        rows: 1,
+    },
+    {
+        img: rearView,
+        title: 'Image 10',
+        cols: 2,
+        rows: 1,
+    },
+    {
+        img: stump,
+        title: 'Image 11',
+        cols: 1,
+        rows: 2,
+    },
+    {
+        img: van,
+        title: 'Image 14',
+        cols: 2,
+        rows: 1,
+    },
+]
+
+export default photoData;
