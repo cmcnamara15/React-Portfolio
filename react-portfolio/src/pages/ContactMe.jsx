9375df6b9ef82b3593a50a505de606eacf5eadce
Chris McNamara <cmcnamara1991@icloud.com>
2023-06-29 12:41:47 -0500

    Add & connect contact form to gmail

--- react-portfolio/src/pages/ContactMe.jsx
+++ react-portfolio/src/pages/ContactMe.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 export default function ContactMe() {
     return (
@@ -7,6 +8,13 @@ export default function ContactMe() {
             <h1>Contact Me</h1>
             <p>(516) 236-7613</p>
             <p>cmcnamara1991@icloud.com</p>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScLxIwbXQmLAPYuj2je_NqLjAEHjaAg4ZIYe2EV_R4zWTd98Q/viewform?embedded=true" 
+            width="640" height="812" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </div>
-    )
+    );
 }
+        
+
+
+
+
