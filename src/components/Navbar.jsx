7a7c71b9758af5b4bd4f0d09a8237eb8c8e9ce79
Chris McNamara <cmcnamara1991@icloud.com>
2023-07-11 18:02:50 -0500

    Remove commented code in navbar.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,26 +49,3 @@ export default function NavBar() {
 
 
 
-
-
-
-
-// import React from "react";
-// import { Tab, Tabs } from '@mui/material';
-// import { useState } from "react";
-
-// export default function Navigation(){
-//     const [value, setValue] = useState("Portfolio")
-//     const handleChange = (event, newValue) => {
-//         setValue(newValue)
-//     };
-//     return(
-//         <Tabs value={value} onChange={handleChange} aria-label="disabled tabs example">
-//             <Tab label="Resume" value = "Resume"/>
-//             <Tab label="Portfolio" value ="Portfolio"/>
-//             <Tab label="AboutMe" value ="AboutMe"/>
-//             <Tab label="ContactMe" value ="ContactMe"/>
-//         </Tabs>
-//     )
-// }
-
